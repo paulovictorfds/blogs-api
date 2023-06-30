82a1d2ef992907e33635104b0a3200084309cc16
Paulo Victor <p.victordsouza@gmail.com>
2023-06-30 13:27:55 -0300

    req 5 finalizado

--- src/middlewares/authenticateToken.js
+++ src/middlewares/authenticateToken.js
@@ -1,14 +1,14 @@
 const { verifyToken } = require('../utils/jwt');
 
 module.exports = ({ headers }, res, next) => {
-  const { token } = headers.authorization;
-  console.log(token);
+  const token = headers.authorization;
+
   if (!token) return res.status(401).json({ message: 'Token not found' });
 
   try {
     verifyToken(token);
   } catch (err) {
-    res.status(401).json({ message: 'Expired or invalid token' });
+    return res.status(401).json({ message: 'Expired or invalid token' });
   }
 
   next();
